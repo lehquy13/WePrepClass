05261a05f3d65085b199c2cd3aeb61f47d97c2cd
lehquy13 <lehquy13@gmail.com>
2025-03-01 13:14:26 +0700

    remove ui app

---
+++ weprepclass-ui/app/auth/layout.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { PropsWithChildren } from 'react';
+
+// Chakra imports
+
+// Layout components
+import FixedPlugin from '@/components/fixedPlugin/FixedPlugin';
+import { isWindowAvailable } from '@/utils/navigation';
+
+// Custom Chakra theme
+
+interface AuthProps extends PropsWithChildren {}
+
+export default function AuthLayout({ children }: AuthProps) {
+  // states and functions
+  if (isWindowAvailable()) document.documentElement.dir = 'ltr';
+  return (
+    <div>
+      <div className="relative float-right h-full min-h-screen w-full dark:!bg-navy-900">
+        <main className={`mx-auto min-h-screen`}>
+          <FixedPlugin />
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ weprepclass-ui/components/link/NavLink.tsx
@@ -0,0 +1,32 @@
+'use client';
+import Link, { LinkProps as NextLinkProps } from 'next/link';
+import {
+  CSSProperties,
+  ComponentProps,
+  PropsWithChildren,
+  useMemo,
+} from 'react';
+
+export type NavLinkProps = NextLinkProps &
+  PropsWithChildren & {
+    styles?: CSSProperties;
+    borderRadius?: ComponentProps<typeof Link>['style']['borderRadius'];
+  };
+
+function NavLink({ className, children, styles, borderRadius, ...props }: any) {
+  const memoizedStyles = useMemo(
+    () => ({
+      borderRadius: borderRadius || 0,
+      ...styles,
+    }),
+    [borderRadius, styles],
+  );
+
+  return (
+    <Link className={`${className}`} style={memoizedStyles} {...props}>
+      {children}
+    </Link>
+  );
+}
+
+export default NavLink;
